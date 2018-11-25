f37088ffd84bbd50a70b85f95ad7312a24134738
icapa <ivan.olea@gmail.com>
2018-11-25 23:48:54 +0100

    Subo configuracion base para firebase

---
+++ src/config/firebase_base.js
@@ -0,0 +1,17 @@
+// Configuracion de firebase
+var config = {
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: ""
+  };
+  var usuario=""
+  var pass=""
+  
+  module.exports = {
+      firebase: config,
+      usuario:usuario,
+      pass: pass
+  }
